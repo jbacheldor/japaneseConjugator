deb3254dea5678a685c51ed6cacada54ea8baf1e
Jess <jlbachel@umich.edu>
2024-08-08 16:06:35 -0400

    adding answers

--- index.jsx
+++ index.jsx
@@ -16,30 +16,65 @@ function createButtons(name, section) {
     section.append(button);
 }
 
-// singular or multple
-function createMenu(question, answers) {
 
+
+function createGenkiMenu(answers) {
+    console.log(answers);
+    bookTitle = document.createElement('p');
+    bookTitle.setAttribute('id','bookTitle');
+    for(var i = 0; i < answers.length; i++){
+        questionAnswer = document.createElement('button');
+        if(i % 2 == 0) {
+            questionAnswer.setAttribute('id', 'bookAnswerEven');
+        }
+        else {
+            questionAnswer.setAttribute('id', 'bookAnswerOdd');
+        }
+        questionAnswer.innerHTML = answers[i];
+        bookTitle.append(questionAnswer);
+        questionAnswer.addEventListener('click', selectElement);
+    }
+
+    menuSection.append(bookTitle);
+}
+
+// singular or multple
+// mayhaps - make this be a switch case - makes incrementing and decrementing ezzz
+function createMenu(question, answers) {
     inputOption = document.createElement('p');
     inputOption.setAttribute('id', 'questionTitle');
     inputOption.innerHTML = question;
     menuSection.append(inputOption);
 
-    for(var i = 0; i < answers.length; i++){
-        questionAnswer = document.createElement('p');
-        questionAnswer.setAttribute('id', 'questionAnswer');
-        questionAnswer.innerHTML = answers[i];
-        menuSection.append(questionAnswer);
-        questionAnswer.addEventListener('click', selectElement);
+    if(question === 'Genki Chapters'){
+        createGenkiMenu(answers);
+    } 
+    else {
+        for(var i = 0; i < answers.length; i++){
+            questionAnswer = document.createElement('p');
+            questionAnswer.setAttribute('id', 'questionAnswer');
+            questionAnswer.innerHTML = answers[i];
+            menuSection.append(questionAnswer);
+            questionAnswer.addEventListener('click', selectElement);
+        }
+    
+        // gotta make it so you can only select all on certain ones 
+    
+        submitButton = document.createElement('button');
+        submitButton.setAttribute('id', 'submit');
+        submitButton.innerHTML = 'submit';
+        menuSection.append(submitButton);
+    
+        submitButton.addEventListener("click", submitInfo);
     }
 
-    // gotta make it so you can only select all on certain ones 
-
-    submitButton = document.createElement('button');
-    submitButton.setAttribute('id', 'submit');
-    submitButton.innerHTML = 'submit';
-    menuSection.append(submitButton);
-
-    submitButton.addEventListener("click", submitInfo);
+    if(!body.querySelector('#back') && question !== 'How would you like to learn?') {
+        const footerSection = document.createElement('div');
+        footerSection.setAttribute('id', 'footerSection');
+        bodySection.append(footerSection);
+        createButtons('back', footerSection);
+        createButtons('next', footerSection);
+    }
 }
 
 function submitInfo(Event){
@@ -51,12 +86,6 @@ function submitInfo(Event){
     }
 
     menuSection.replaceChildren(); 
-
-    const footerSection = document.createElement('div');
-    footerSection.setAttribute('id', 'footerSection');
-    bodySection.append(footerSection);
-    createButtons('back', footerSection);
-    createButtons('next', footerSection);
 
     console.log(newList);
     if(newList.includes('Build your Own')){
