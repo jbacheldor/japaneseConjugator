308cf90a00d123c577b72de26865ada5b7a6e0a4
Jess <jlbachel@umich.edu>
2025-03-11 22:07:07 -0400

    successfully divorced the pages from each other :-)

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { clearValues1, keysFunction } from "./pages/QuizPage.js";
+import { createQuizPage } from "./pages/QuizPage.js";
 
 const body = document.querySelector("#body");
 const menuSection = document.querySelector(".input-tester");
@@ -10,9 +10,9 @@ const howToStudy = ['Follow a Course', "Build your Own"];
 const verbForms = ['Te Form', 'Present Affirmative', 'Present Negative', 'Past Affirmative', 'Past Negative', 'Long Form', 'Short Form'];
 const adjectiveForms = ['Na', 'I'];
 
-let currentElement = "";
+// let currentElement = "";
 
-let answerValues = []
+// let answerValues = []
 
 function createButtons(name, section, onClickFunc) {
     const button = document.createElement('button');
@@ -64,17 +64,22 @@ function goBack() {
 
 function createGenkiMenu(answers) {
     // genki 1 and genki 2
-    bookTitle = document.createElement('div');
+    let bookTitle = document.createElement('div');
     bookTitle.setAttribute('id', 'bookTitle');
     for (let i = 0; i < answers.length; i++) {
-        questionAnswer = document.createElement('button');
+        let questionAnswer = document.createElement('button');
+        if (i % 2 == 0) {
+            questionAnswer.setAttribute('id', 'bookAnswerEven');
+        } else {
+            questionAnswer.setAttribute('id', 'bookAnswerOdd');
+        }
         questionAnswer.innerHTML = answers[i];
         bookTitle.append(questionAnswer);
         questionAnswer.addEventListener('click', selectMultipleElement);
     }
     menuSection.append(bookTitle);
 
-    inputOption = document.createElement('p');
+    let inputOption = document.createElement('p');
     inputOption.setAttribute('id', 'questionTitle');
     inputOption.innerHTML = 'Genki 2 Chapters';
     menuSection.append(inputOption);
@@ -82,7 +87,7 @@ function createGenkiMenu(answers) {
     bookTitle = document.createElement('p');
     bookTitle.setAttribute('id', 'bookTitle');
     for (let i = 0; i < answers.length; i++) {
-        questionAnswer = document.createElement('button');
+        let questionAnswer = document.createElement('button');
         if (i % 2 == 0) {
             questionAnswer.setAttribute('id', 'bookAnswerEven');
         } else {
@@ -97,229 +102,13 @@ function createGenkiMenu(answers) {
     createSubmitButton();
 }
 
-function createQuizPage() {
-    // u must remove the input tester body
-    const bodySection = document.querySelector(".bodySection");
-    const inputTester = bodySection.querySelector(".input-tester");
-    bodySection.removeChild(inputTester)
-
-    // change background
-    let background = document.querySelector('#background');
-    background.style.backgroundImage = "url(./svgs/Background_Quiz.svg)";
-    background.removeChild(document.querySelector("#header"));
-
-    // sets the top part of the quiz page 
-    let topQuiz = document.createElement('div');
-    topQuiz.setAttribute('id', 'topQuiz')
-
-    let home = document.createElement('button');
-    home.setAttribute('id', 'homeButton');
-    home.innerText = "home"
-    topQuiz.append(home)
-
-    let form = document.createElement('span');
-    form.setAttribute('id', 'formTitle');
-    form.innerHTML = "{Form Here}"
-    topQuiz.append(form)
-
-    let tutorial = document.createElement('button');
-    tutorial.setAttribute('id', 'tutorialButton');
-    tutorial.innerText = "tutorial"
-    topQuiz.append(tutorial)
-    bodySection.append(topQuiz)
-
-    // add in the target value
-    let mainBlock = document.createElement('div');
-    mainBlock.setAttribute('id', 'mainBlock');
-
-    // add in remaining amount
-    let remaining = document.createElement('div');
-    remaining.setAttribute('id', 'remainingQuestions');
-    remaining.innerHTML = `remaining / total`
-
-    mainBlock.append(remaining);
-
-    let centerBlock = document.createElement('div');
-    centerBlock.setAttribute('id', 'centerBlock');
-
-    let superScript = document.createElement('span');
-    superScript.setAttribute('id', 'superScript');
-    superScript.innerHTML = "た"
-    centerBlock.append(superScript);
-
-    let kanjiBlock = document.createElement('span');
-    kanjiBlock.setAttribute('id', 'kanjiBlock');
-    kanjiBlock.innerHTML = "食べる"
-    centerBlock.append(kanjiBlock);
-    // centerBlock.style.width = `${kanjiBlock.innerHTML.length*20}px`
-    mainBlock.append(centerBlock);
-
-    let translated = document.createElement('span');
-    translated.setAttribute('id', 'translated');
-    translated.innerHTML = "v. to eat"
-    mainBlock.append(translated);
-
-    bodySection.append(mainBlock)
-
-    // add in the user value inputs 
-    let inputContainer = document.createElement('div');
-    inputContainer.setAttribute('id', 'containerInputBox');
-
-    let inputBox = document.createElement('div');
-    inputBox.setAttribute('id', 'inputBox');
-
-    inputContainer.append(inputBox);
-
-
-    let clearButton = document.createElement('button');
-    clearButton.setAttribute('id', 'clearButton');
-    clearButton.innerText = "clear"
-    clearButton.addEventListener('click', clearValues)
-    inputBox.append(clearButton)
-
-
-    let listOfTiles = ["た", "べ", "ま", "す"]
-
-
-    for (let i = 0; i < listOfTiles.length; i++) {
-        inputBox.append(createInputTiles(listOfTiles[i]))
-    }
-
-    inputContainer.append(inputBox)
-
-    bodySection.append(inputContainer)
-
-    // now we need the input button-y section
-
-    let instruction = document.createElement('span')
-    instruction.setAttribute('id', 'instruction')
-    instruction.innerHTML = "type or select from below"
-    bodySection.append(instruction)
-
-    // set the special characters
-
-    let specialCharacters = document.createElement('div');
-    specialCharacters.setAttribute('id', 'specialCharacters');
-
-    let specialCharactersList = ["ゃ", "ゅ", "ょ", "っ", "ー", "゛", "゜"]
-
-    for (let i = 0; i < specialCharactersList.length; i++) {
-        let specialCharacter = document.createElement('div');
-        specialCharacter.setAttribute('class', 'activeTiles');
-        specialCharacter.innerHTML = specialCharactersList[i]
-        specialCharacters.append(specialCharacter)
-    }
-
-    bodySection.append(specialCharacters)
-
-    // set the non-special characters
-    let regularCharactersList = [
-        ["あ", "い", "う", "お", "え"],
-        ["か", "き", "く", "こ", "け"],
-        ["さ", "し", "す", "そ", "せ"],
-        ["た", "つ", "ち", "と", "て"],
-        ["な", "に", "ぬ", "の", "ね"],
-        ["は", "ひ", "ふ", "ほ", "へ"],
-        ["ま", "み", "む", "も", "め"],
-        ["や", "", "ゆ", "よ", ""],
-        ["ら", "り", "る", "ろ", "れ"],
-        ["わ", "ん", "", "を", ""]
-    ]
-    let regularCharacters = document.createElement('div');
-    regularCharacters.setAttribute('id', 'regularCharacters');
-
-    regularCharactersList.forEach((column) => {
-        let characterColumn = document.createElement('div');
-        characterColumn.setAttribute('class', 'characterColumn');
-
-        column.forEach((item) => {
-            let regularCharacter = document.createElement('div');
-            regularCharacter.setAttribute('class', item == "" ? 'emptyTiles' : 'activeTiles');
-            regularCharacter.innerHTML = item
-            regularCharacter.addEventListener('click', e => updateInput(e))
-            characterColumn.append(regularCharacter)
-        })
-
-        regularCharacters.append(characterColumn)
-    })
-
-    bodySection.append(regularCharacters)
-
-    // submit button
-
-    let submitButton = document.createElement('button');
-    submitButton.setAttribute('id', 'submit');
-    submitButton.innerHTML = "submit"
-    submitButton.addEventListener("click", submitValues)
-
-    bodySection.append(submitButton)
-}
-
-function clearValues() {
-    clearValues1();
-    // parentComponent = document.querySelector('#inputBox').children;
-    // for(let i = 0; i < parentComponent.length; i++){
-    //     if(parentComponent[i].nodeName == "INPUT"){
-    //         parentComponent[i].value = ""
-    //     }
-    // }
-}
-
-function submitValues() {
-    answerValues.push({
-        word: '',
-        form: '',
-        answer: '',
-        type: ''
-    })
-    console.log('answer values', answerValues)
-}
-
-function updateInput(event) {
-    const target = document.getElementById(currentElement.id)
-    target.value = event.target.innerHTML
-}
-
-function setId(id) {
-    id.setAttribute('selected', "true");
-
-    if (currentElement) {
-        let prevElement = document.getElementById(currentElement.id);
-        prevElement.removeAttribute('selected');
-    }
-
-    currentElement = id;
-}
-
-// need to have this separate because the input tiles will be dynamically rendered each time i think
-function createInputTiles(value) {
-    let inputTiles = document.createElement('input');
-    inputTiles.setAttribute('class', 'inputTiles');
-    inputTiles.setAttribute('id', `${Math.random()}`)
-    inputTiles.value = value
-
-    inputTiles.addEventListener("click", (e) => {
-        setId(e.target)
-    })
-
-    inputTiles.addEventListener("keydown", e => letterInput(e))
-    return inputTiles
-}
-
-
-function letterInput(e) {
-    let focusElement = document.getElementById(currentElement.id)
-    e.preventDefault()
-    keysFunction(e, focusElement, setId)
-}
-
 
 function createResultsPage() {
 
 }
 
 function createSubmitButton() {
-    submitButton = document.createElement('button');
+    let submitButton = document.createElement('button');
     submitButton.setAttribute('id', 'submit');
     submitButton.innerHTML = 'submit';
     menuSection.append(submitButton);
@@ -371,7 +160,7 @@ function figureOutPage(pageNumber) {
 // singular or multple
 // mayhaps - make this be a switch case - makes incrementing and decrementing ezzz
 function createMenu(question, answers) {
-    inputOption = document.createElement('p');
+    let inputOption = document.createElement('p');
     inputOption.setAttribute('id', 'questionTitle');
     inputOption.innerHTML = question;
     menuSection.append(inputOption);
@@ -380,7 +169,7 @@ function createMenu(question, answers) {
         createGenkiMenu(answers);
     } else {
         for (let i = 0; i < answers.length; i++) {
-            questionAnswer = document.createElement('p');
+            let questionAnswer = document.createElement('p');
             questionAnswer.setAttribute('id', 'questionAnswer');
             questionAnswer.innerHTML = answers[i];
             menuSection.append(questionAnswer);
@@ -463,17 +252,6 @@ function selectMultipleElement(Event) {
     }
 }
 
-
-// // add event listen f
-
-// function createNext( {
-
-// })
-
 // createMenu('How would you like to learn?', howToStudy);
 
 createQuizPage()
-
-export function test() {
-
-}
--- pages/QuizPage.js
+++ pages/QuizPage.js
@@ -1,4 +1,8 @@
-export function clearValues1() {
+let currentElement = "";
+
+let answerValues = []
+
+export function clearValues() {
     let parentComponent = document.querySelector('#inputBox').children;
     for (let i = 0; i < parentComponent.length; i++) {
         if (parentComponent[i].nodeName == "INPUT") {
@@ -82,6 +86,19 @@ export function keysFunction(e, focusElement, setId) {
                     case 'k':
                         focusElement.value = 'き'
                         break;
+                    case 'g':
+                        focusElement.value = 'ぎ'
+                        break;
+                    case 'j':
+                        //  ぢ oooh no this is also ji
+                        focusElement.value = 'じ'
+                        break;
+                    case 'b':
+                        focusElement.value = 'び'
+                        break;
+                    case 'p':
+                        focusElement.value = 'ぴ'
+                        break;
                     case 'ん':
                     case 'n':
                         focusElement.value = 'に'
@@ -110,11 +127,26 @@ export function keysFunction(e, focusElement, setId) {
                     case 'k':
                         focusElement.value = 'か'
                         break;
+                    case 'g':
+                        focusElement.value = 'が'
+                        break;
                     case 's':
                         focusElement.value = 'さ'
                         break;
+                    case 'z':
+                        focusElement.value = 'ざ'
+                        break;
                     case 't':
                         focusElement.value = 'た'
+                        break;
+                    case 'd':
+                        focusElement.value = 'だ'
+                        break;
+                    case 'b':
+                        focusElement.value = 'ば'
+                        break;
+                    case 'p':
+                        focusElement.value = 'ぱ'
                         break;
                     case 'ん':
                     case 'n':
@@ -147,11 +179,26 @@ export function keysFunction(e, focusElement, setId) {
                     case 'k':
                         focusElement.value = 'こ'
                         break;
+                    case 'g':
+                        focusElement.value = 'ご'
+                        break;
                     case 's':
                         focusElement.value = 'そ'
                         break;
+                    case 'z':
+                        focusElement.value = 'ぞ'
+                        break;
+                    case 'd':
+                        focusElement.value = 'ど'
+                        break;
                     case 't':
                         focusElement.value = 'と'
+                        break;
+                    case 'b':
+                        focusElement.value = 'ぼ'
+                        break;
+                    case 'p':
+                        focusElement.value = 'ぽ'
                         break;
                     case 'ん':
                     case 'n':
@@ -184,11 +231,24 @@ export function keysFunction(e, focusElement, setId) {
                     case 'k':
                         focusElement.value = 'く'
                         break;
+                    case 'g':
+                        focusElement.value = 'ぐ'
+                        break;
                     case 's':
                         focusElement.value = 'す'
                         break;
+                    case 'z':
+                        //  there are doubles づ 
+                        focusElement.value = 'ず'
+                        break;
                     case 't':
                         focusElement.value = 'ち'
+                        break;
+                    case 'b':
+                        focusElement.value = 'ぶ'
+                        break;
+                    case 'p':
+                        focusElement.value = 'ぷ'
                         break;
                     case 'ん':
                     case 'n':
@@ -223,11 +283,26 @@ export function keysFunction(e, focusElement, setId) {
                     case 'k':
                         focusElement.value = 'け'
                         break;
+                    case 'g':
+                        focusElement.value = 'げ'
+                        break;
                     case 's':
                         focusElement.value = 'せ'
                         break;
+                    case 'd':
+                        focusElement.value = 'で'
+                        break;
+                    case 'z':
+                        focusElement.value = 'ぜ'
+                        break;
                     case 't':
                         focusElement.value = 'て'
+                        break;
+                    case 'b':
+                        focusElement.value = 'べ'
+                        break;
+                    case 'p':
+                        focusElement.value = 'ぺ'
                         break;
                     case 'ん':
                     case 'n':
@@ -261,10 +336,10 @@ export function keysFunction(e, focusElement, setId) {
 
             // j baby
         case 74:
-            if (focusElement.value.length == 0) {
-                focusElement.value = 'え'
-            }
-            break;
+            // if (focusElement.value.length == 0) {
+            //     focusElement.value = 'え'
+            // }
+            // break;
             // g 
         case 71:
 
@@ -289,4 +364,214 @@ export function keysFunction(e, focusElement, setId) {
         default:
             focusElement.value = e.key
     }
+}
+
+
+
+export function createQuizPage() {
+    // u must remove the input tester body
+    const bodySection = document.querySelector(".bodySection");
+    const inputTester = bodySection.querySelector(".input-tester");
+    bodySection.removeChild(inputTester)
+
+    // change background
+    let background = document.querySelector('#background');
+    background.style.backgroundImage = "url(./svgs/Background_Quiz.svg)";
+    background.removeChild(document.querySelector("#header"));
+
+    // sets the top part of the quiz page 
+    let topQuiz = document.createElement('div');
+    topQuiz.setAttribute('id', 'topQuiz')
+
+    let home = document.createElement('button');
+    home.setAttribute('id', 'homeButton');
+    home.innerText = "home"
+    topQuiz.append(home)
+
+    let form = document.createElement('span');
+    form.setAttribute('id', 'formTitle');
+    form.innerHTML = "{Form Here}"
+    topQuiz.append(form)
+
+    let tutorial = document.createElement('button');
+    tutorial.setAttribute('id', 'tutorialButton');
+    tutorial.innerText = "tutorial"
+    topQuiz.append(tutorial)
+    bodySection.append(topQuiz)
+
+    // add in the target value
+    let mainBlock = document.createElement('div');
+    mainBlock.setAttribute('id', 'mainBlock');
+
+    // add in remaining amount
+    let remaining = document.createElement('div');
+    remaining.setAttribute('id', 'remainingQuestions');
+    remaining.innerHTML = `remaining / total`
+
+    mainBlock.append(remaining);
+
+    let centerBlock = document.createElement('div');
+    centerBlock.setAttribute('id', 'centerBlock');
+
+    let superScript = document.createElement('span');
+    superScript.setAttribute('id', 'superScript');
+    superScript.innerHTML = "た"
+    centerBlock.append(superScript);
+
+    let kanjiBlock = document.createElement('span');
+    kanjiBlock.setAttribute('id', 'kanjiBlock');
+    kanjiBlock.innerHTML = "食べる"
+    centerBlock.append(kanjiBlock);
+    // centerBlock.style.width = `${kanjiBlock.innerHTML.length*20}px`
+    mainBlock.append(centerBlock);
+
+    let translated = document.createElement('span');
+    translated.setAttribute('id', 'translated');
+    translated.innerHTML = "v. to eat"
+    mainBlock.append(translated);
+
+    bodySection.append(mainBlock)
+
+    // add in the user value inputs 
+    let inputContainer = document.createElement('div');
+    inputContainer.setAttribute('id', 'containerInputBox');
+
+    let inputBox = document.createElement('div');
+    inputBox.setAttribute('id', 'inputBox');
+
+    inputContainer.append(inputBox);
+
+
+    let clearButton = document.createElement('button');
+    clearButton.setAttribute('id', 'clearButton');
+    clearButton.innerText = "clear"
+    clearButton.addEventListener('click', clearValues)
+    inputBox.append(clearButton)
+
+
+    let listOfTiles = ["た", "べ", "ま", "す"]
+
+
+    for (let i = 0; i < listOfTiles.length; i++) {
+        inputBox.append(createInputTiles(listOfTiles[i]))
+    }
+
+    inputContainer.append(inputBox)
+
+    bodySection.append(inputContainer)
+
+    // now we need the input button-y section
+
+    let instruction = document.createElement('span')
+    instruction.setAttribute('id', 'instruction')
+    instruction.innerHTML = "type or select from below"
+    bodySection.append(instruction)
+
+    // set the special characters
+
+    let specialCharacters = document.createElement('div');
+    specialCharacters.setAttribute('id', 'specialCharacters');
+
+    let specialCharactersList = ["ゃ", "ゅ", "ょ", "っ", "ー", "゛", "゜"]
+
+    for (let i = 0; i < specialCharactersList.length; i++) {
+        let specialCharacter = document.createElement('div');
+        specialCharacter.setAttribute('class', 'activeTiles');
+        specialCharacter.innerHTML = specialCharactersList[i]
+        specialCharacters.append(specialCharacter)
+    }
+
+    bodySection.append(specialCharacters)
+
+    // set the non-special characters
+    let regularCharactersList = [
+        ["あ", "い", "う", "お", "え"],
+        ["か", "き", "く", "こ", "け"],
+        ["さ", "し", "す", "そ", "せ"],
+        ["た", "つ", "ち", "と", "て"],
+        ["な", "に", "ぬ", "の", "ね"],
+        ["は", "ひ", "ふ", "ほ", "へ"],
+        ["ま", "み", "む", "も", "め"],
+        ["や", "", "ゆ", "よ", ""],
+        ["ら", "り", "る", "ろ", "れ"],
+        ["わ", "ん", "", "を", ""]
+    ]
+    let regularCharacters = document.createElement('div');
+    regularCharacters.setAttribute('id', 'regularCharacters');
+
+    regularCharactersList.forEach((column) => {
+        let characterColumn = document.createElement('div');
+        characterColumn.setAttribute('class', 'characterColumn');
+
+        column.forEach((item) => {
+            let regularCharacter = document.createElement('div');
+            regularCharacter.setAttribute('class', item == "" ? 'emptyTiles' : 'activeTiles');
+            regularCharacter.innerHTML = item
+            regularCharacter.addEventListener('click', e => updateInput(e))
+            characterColumn.append(regularCharacter)
+        })
+
+        regularCharacters.append(characterColumn)
+    })
+
+    bodySection.append(regularCharacters)
+
+    // submit button
+
+    let submitButton = document.createElement('button');
+    submitButton.setAttribute('id', 'submit');
+    submitButton.innerHTML = "submit"
+    submitButton.addEventListener("click", submitValues)
+
+    bodySection.append(submitButton)
+}
+
+function submitValues() {
+    answerValues.push({
+        word: '',
+        form: '',
+        answer: '',
+        type: ''
+    })
+    console.log('answer values', answerValues)
+}
+
+function updateInput(event) {
+    const target = document.getElementById(currentElement.id)
+    target.value = event.target.innerHTML
+}
+
+
+// need to have this separate because the input tiles will be dynamically rendered each time i think
+function createInputTiles(value) {
+    let inputTiles = document.createElement('input');
+    inputTiles.setAttribute('class', 'inputTiles');
+    inputTiles.setAttribute('id', `${Math.random()}`)
+    inputTiles.value = value
+
+    inputTiles.addEventListener("click", (e) => {
+        setId(e.target)
+    })
+
+    inputTiles.addEventListener("keydown", e => letterInput(e))
+    return inputTiles
+}
+
+
+function setId(id) {
+    id.setAttribute('selected', "true");
+
+    if (currentElement) {
+        let prevElement = document.getElementById(currentElement.id);
+        prevElement.removeAttribute('selected');
+    }
+
+    currentElement = id;
+}
+
+
+function letterInput(e) {
+    let focusElement = document.getElementById(currentElement.id)
+    e.preventDefault()
+    keysFunction(e, focusElement, setId)
 }
